260769dcc9ed046a2e9b1d1d492fa0d74f954c41
Sven Korset <sven.korset@indie-software.de>
2020-11-22 12:21:20 +0100

    SPM Platform flag

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NavigationStack",
+    platforms: [.iOS("13.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
