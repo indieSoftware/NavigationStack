7aa2d556a6101c9f214ce85c062e4adc159492f0
Sven Korset <sven.korset@indie-software.de>
2021-04-11 16:08:02 +0200

    Experiment refined

--- Sources/NavigationStackExample/Experiments/Experiment10.swift
+++ Sources/NavigationStackExample/Experiments/Experiment10.swift
@@ -5,29 +5,23 @@ import SwiftUI
 
 struct Experiment10: View {
 	var body: some View {
-		if #available(iOS 14.0, *) {
-			ContentView()
-				.environmentObject(NavigationModel())
-		} else {
-			Text("iOS 14+ required")
-		}
+		ContentView()
+			.environmentObject(NavigationModel())
 	}
 }
 
-@available(iOS 14.0, *)
-private struct FirstStep: View {
+private struct SecondScreen: View {
 	@EnvironmentObject var navigationModel: NavigationModel
 	var body: some View {
 		ZStack {
-			Color.blue.ignoresSafeArea()
+			Color.blue
 			VStack {
 				Spacer()
 				HStack {
 					Spacer()
 					VStack {
-						Text("First Step")
+						Text("Screen 2")
 							.foregroundColor(.white)
-							.padding(.bottom, 40)
 						Button(action: {
 							print("NavStack: \(navigationModel)")
 						}, label: {
@@ -43,18 +37,9 @@ private struct FirstStep: View {
 	}
 }
 
-@available(iOS 14.0, *)
 private struct ContentView: View {
 	static let id = String(describing: Self.self)
 	@EnvironmentObject var navigationModel: NavigationModel
-	func showFirstStep() {
-		DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-			print("called me")
-			self.navigationModel.pushContent(ContentView.id) {
-				FirstStep()
-			}
-		}
-	}
 
 	var body: some View {
 		NavigationStackView(ContentView.id) {
@@ -65,7 +50,7 @@ private struct ContentView: View {
 					HStack {
 						Spacer()
 						VStack {
-							Text("test")
+							Text("Screen 1")
 							Button(action: {
 								print("NavStack: \(navigationModel)")
 							}, label: {
@@ -79,7 +64,12 @@ private struct ContentView: View {
 			}.edgesIgnoringSafeArea(.all)
 		}
 		.onAppear {
-			showFirstStep()
+			DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+				print("Push executed")
+				self.navigationModel.pushContent(ContentView.id) {
+					SecondScreen()
+				}
+			}
 		}
 	}
 }
