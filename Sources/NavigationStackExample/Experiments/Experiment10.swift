5bcd77fce8f2a94f9330a8dbb18bdd519619a486
Sven Korset <sven.korset@indie-software.de>
2021-06-13 18:21:37 +0200

    Experiment documentation updated, it seems this bug is fixed with iOS 14.5

--- Sources/NavigationStackExample/Experiments/Experiment10.swift
+++ Sources/NavigationStackExample/Experiments/Experiment10.swift
@@ -1,10 +1,12 @@
 // Issue report: https://github.com/indieSoftware/NavigationStack/issues/1
 // With v1.0.2 the push is not visually executed sometimes.
-// When starting the app multiple times then sometimes the animation doesn't apply
+// Could be reproduced with iOS 14.4, but not anymore with iOS 14.5.
+// When starting the app with this text multiple times then sometimes the animation doesn't apply
 // and the pushed screen is not visible even when the nav stack is correclty set after the called push.
 // It seems this happens because of `isAlternativeViewShowingPrecede` is set to true in `NavigationStackModel`
 // and immediately after that `isAlternativeViewShowing` is also set to true within a `withAnimation` block.
 // When wrapping the last assignment inclusive the `withAnimation` block into a `DispatchQueue.main.asyncAfter(deadline: .now())` seems to solve this issue.
+// It seems with iOS 14.5 it's also solved.
 
 import NavigationStack
 import SwiftUI
